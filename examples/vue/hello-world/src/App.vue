599721276c9d00e0d411e5f7e331d37f29dcc1f8
Tomasz Lukawski <t.lukawski@samsung.com>
2020-07-03 08:52:28 +0200

    Update example - disable page refresh
    [Issue] https://github.com/TomaszLukawskiSam/TAU/issues/33
    [Problem] Update vanillaJs example
    [Solution]
     - vanilla js: changes from gh-pages have been moved to examples in TAU
     - vue: added style to body
     - react: added style to body
     - angular: added style to body
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- examples/vue/hello-world/src/App.vue
+++ examples/vue/hello-world/src/App.vue
@@ -20,6 +20,9 @@ export default {
 </script>
 
 <style>
+body {
+  overscroll-behavior: contain;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,4 +31,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
