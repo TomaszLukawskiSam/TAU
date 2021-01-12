ee281d6061da36049fd4a40b8ef3b5b7ec03e208
singa2000 <cookie@samsung.com>
2021-01-12 10:19:00 +0900

    [WatchTogether] Add the WatchTogether Sample
    [Issue] N/A
    [Problem] There is no WatchTogether Sample
    [Solution] Add the WatchTogether Sample for TV-Mobile D2D App
    
    Signed-off-by: singa2000 <cookie@samsung.com>

---
+++ examples/mobile/WatchTogether/js/d2dServiceLocal.js
@@ -0,0 +1,42 @@
+class D2DServiceLocal {
+    constructor() {
+        this.initialize();
+    }
+    initialize() {
+        this.wsBase = "ws://localhost:";
+        this.defaultPort = "9000";
+        this.pkgId = "NAe4rftRic";
+        this.port = null;
+        this.TO_ALL = 100; // Assume that only 100 clients can be connected to a web socket server
+        this.websocket = null;
+        this.url = null;
+        this.SERVER = -1;
+
+        this.clients = [];
+        // extract a port number from query and apply it on wsUri
+        this.url = document.location.href;
+        this.qs = this.url.substring(this.url.indexOf('?') + 1).split('&');
+        for (var i = 0, result = {}; i < this.qs.length; i++) {
+            this.qs[i] = this.qs[i].split('=');
+            result[this.qs[i][0]] = decodeURIComponent(this.qs[i][1]);
+        }
+        this.port = result.port;
+        this.wsUri = this.wsBase.concat((this.port) ? this.port : this.defaultPort, "/", this.pkgId);
+        this.websocket = new WebSocket(this.wsUri);
+        this.websocket.onmessage = function(evt) {
+            var msg = JSON.parse(evt.data);
+            if (msg.id == this.SERVER && msg.type == "new_client")
+                this.clients.push(msg.data);
+            onMessage(evt);
+        };
+    }
+
+    doSend(type, data, whom = this.TO_ALL) {
+        var packet = {type: type, data: data, id: whom};
+        this.websocket.send(JSON.stringify(packet));
+    }
+
+    sendMessage(what, data, whom = this.TO_ALL) {
+        this.doSend(what, data, whom);
+    }
+}
