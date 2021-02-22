36cfcccdfebcec9a19f3ddeb5274bcf4cd439d65
Tomasz Lukawski <t.lukawski@samsung.com>
2021-02-22 15:18:33 +0100

    SearchBar: add search bar template
    [Issue] https://github.com/Samsung/TAU/issues/1585
    [Problem] Search bar template is not implemented
    [Solution]
     - new template has been added
     - new styles for appbar with search bar
     - clear button style update
     - search images update
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

---
+++ examples/mobile/UIComponents/components/search/search-bar.js
@@ -0,0 +1,25 @@
+(function (document, tau) {
+	var page = document.getElementById("search-bar-page"),
+		search;
+
+	function onSearchFocus() {
+		var popup = tau.widget.Popup(page.querySelector(".ui-popup"));
+
+		popup.open();
+	}
+
+	function onPageShow() {
+		search = page.querySelector("header .ui-appbar-title-container input[type='search']");
+		search.addEventListener("focus", onSearchFocus);
+	}
+
+	function onPageHide(ev) {
+		var page = ev.target;
+
+		page.removeEventListener("pageshow", onPageShow);
+	}
+
+	page.addEventListener("pageshow", onPageShow);
+	page.addEventListener("pagehide", onPageHide);
+
+}(window.document, window.tau));
